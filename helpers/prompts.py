96e04587bf7c9b97d7b63c85b7a2c4d98d96c561
Joshua-K1 <joshuack1996@gmail.com>
2024-02-04 22:06:23 +0000

    x

--- helpers/prompts.py
+++ helpers/prompts.py
@@ -3,7 +3,13 @@ from logger.logger import event_logger
 
 def read_prompts(prompt_type):
     event_logger.info("Searching Prompt File")
-    with open('system_prompts/prompts.json', 'r', encoding='utf-8') as prompt_file:
+    try: 
+     with open('system_prompts/prompts.json', 'r', encoding='utf-8') as prompt_file:
         prompts = json.load(prompt_file)
 
         return prompts[prompt_type]
+
+    except(ValueError, FileNotFoundError) as err:
+      event_logger.error(err)
+      event_logger.error("Prompt file could not be found or is in an invalid format")
+
